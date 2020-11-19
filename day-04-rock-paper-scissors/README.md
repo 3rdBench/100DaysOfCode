00a03ba04c80f509af1677e5f9939d4bb77aac8b
3rdBench <benjie.work@gmail.com>
2020-11-19 23:26:46 +0800

    Initial commit on day 04 project

---
+++ day-04-rock-paper-scissors/README.md
@@ -0,0 +1,21 @@
+## Rock, Paper, Scissors Game
+
+A simple text-based version of the 'Rock, Paper, Scrissors' game.
+
+### What does it do?
+
+The user is asked for a choice between Rock, Paper or Scissors
+* The program provides the choice as whole numbers between 0 to 2 (0 = Rock, 1 = Paper, 2 = Scissors)
+* User the corresponding choice
+* The programs generates its own choice
+* Corresponding hand gestures (ASCII art) and result are displayed on the console
+
+### How does it work?
+
+The program uses Python's:
+* if-else, elif statements
+* input() function
+* list
+* logical operators
+* random module
+* randint() method
---
+++ day-04-rock-paper-scissors/main.py
@@ -0,0 +1,58 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+import random
+
+# 3 Simple Rules (https://www.wrpsa.com/the-official-rules-of-rock-paper-scissors/):
+# Rock wins against Scissors
+# Scissors wins against Paper
+# Paper wins against Rock
+
+# Save choices to a list
+hand_gesture = [rock, paper, scissors]
+
+# Display how to play the game & get the user's choice
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+print(hand_gesture[user_choice])
+
+# Generate the computer's choice
+computer_choice = random.randint(0,2)
+
+print("Computer chose:\n")
+print(hand_gesture[computer_choice])
+
+if user_choice == computer_choice:
+    print("It is a tie.") 
+
+if (user_choice == 0) and (computer_choice == 2):
+    print("You win.")
+elif (user_choice == 2) and (computer_choice == 1):
+    print("You win.")
+elif (user_choice == 1) and (computer_choice == 0):
+    print("You win.")
+else:
+    print("You lose.")
